75d10c4e4b1eb0bc0c9ac6578ab6eb419ce9a75d
srikanthganta182 <srikanth.ganta@ixor.be>
2024-03-03 14:55:41 +0100

    Basic template for future

--- local.sh
+++ local.sh
@@ -1,6 +1,6 @@
 # build docker 
-docker build -t spleeter-serverless-docker .
+docker build -t runpod-serverless-docker .
 #  remove any docker container with the same name IF Exists
-/usr/local/bin/docker rm -f spleeter-container || true
+/usr/local/bin/docker rm -f runpod-serverless-docker-container || true
 # run docker container
-/usr/local/bin/docker  run -d --name spleeter-container -p 80:80 -e LOCAL=True spleeter-serverless-docker
+/usr/local/bin/docker  run -d --name runpod-serverless-docker-container -p 80:80 -e LOCAL=True runpod-serverless-docker
